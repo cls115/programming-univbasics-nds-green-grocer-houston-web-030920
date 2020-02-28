221f7549708480b39ca754a12189170caa2b9658
Colton stephens <stephens.colton@icloud.com>
2020-02-28 00:47:42 +0000

    Automatically backed up by Learn

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -1,1 +1,11 @@
-
+def increment_count_of_item (cart, item_name)
+  i = 0
+  while i < cart.size do
+    current_item = cart[i]
+    if ( current_item[:item] == item_name )
+      current_item[:count] += 1
+    end
+    i += 1
+  end
+  cart
+end
