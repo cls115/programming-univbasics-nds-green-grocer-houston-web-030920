4c71521af1c0a0e223d6c577490d9db69e8b6b24
Colton stephens <stephens.colton@icloud.com>
2020-02-28 00:48:47 +0000

    Automatically backed up by Learn

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -93,7 +93,6 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-@@ -33,4 +102,21 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
